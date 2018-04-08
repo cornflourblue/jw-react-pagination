aef2311de0d4c88ee3ec10e7a6103cec95255e3b
Jason Watmore <jason@pointblankdevelopment.com.au>
2018-04-08 12:22:31 +1000

    removed lodash dependency

--- src/JwPagination.jsx
+++ src/JwPagination.jsx
@@ -126,7 +126,7 @@ class JwPagination extends React.Component {
         var endIndex = Math.min(startIndex + pageSize - 1, totalItems - 1);
 
         // create an array of pages to ng-repeat in the pager control
-        var pages = _.range(startPage, endPage + 1);
+        var pages = [...Array((endPage + 1) - startPage).keys()].map(i => startPage + i);
 
         // return object with all pager properties required by the view
         return {
